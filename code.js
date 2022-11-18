c49fc4a7390ae09ea22f083795e01bf93400adb7
XINEGAMES <117596203+XINEGAMES@users.noreply.github.com>
2022-11-18 17:40:17 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,182 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":["848314f8-d7dc-4755-a4a1-3782173096b1","26b80e63-bc0f-408d-b288-be2282aebd4e","dfc53be7-786c-4305-b8eb-81fa595e9903","47fade86-5bd1-4789-87af-896fb1433a7b","043deebf-64b8-4795-be8d-db5055414f2f","c26b6449-0b6c-4483-8486-107c0091c26e","3221caad-ae37-4eec-92a8-56bae4769e66","c4e310e0-8174-4127-a46e-0cbcc94b488b","948cf14d-e690-40c9-85e8-9bdb31c4fb01"],"propsByKey":{"848314f8-d7dc-4755-a4a1-3782173096b1":{"name":"hero","sourceUrl":null,"frameSize":{"x":30,"y":30},"frameCount":1,"looping":true,"frameDelay":12,"version":"xdUKXIB4u03efSuHDAWNliF3hxnAOcVh","categories":["sports"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":30,"y":30},"rootRelativePath":"assets/848314f8-d7dc-4755-a4a1-3782173096b1.png"},"26b80e63-bc0f-408d-b288-be2282aebd4e":{"name":"enemy1","sourceUrl":null,"frameSize":{"x":35,"y":50},"frameCount":1,"looping":true,"frameDelay":12,"version":"cPVbQA5S8MXe64CMIP5GbJVXO7ECEg39","categories":["icons"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":35,"y":50},"rootRelativePath":"assets/26b80e63-bc0f-408d-b288-be2282aebd4e.png"},"dfc53be7-786c-4305-b8eb-81fa595e9903":{"name":"enemy","sourceUrl":"assets/api/v1/animation-library/gamelab/xasculQGiYxBV79ltD_0E79ZRIexdPdZ/category_food/american_hamburger.png","frameSize":{"x":320,"y":254},"frameCount":1,"looping":true,"frameDelay":2,"version":"xasculQGiYxBV79ltD_0E79ZRIexdPdZ","categories":["food"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":320,"y":254},"rootRelativePath":"assets/api/v1/animation-library/gamelab/xasculQGiYxBV79ltD_0E79ZRIexdPdZ/category_food/american_hamburger.png"},"47fade86-5bd1-4789-87af-896fb1433a7b":{"name":"enemy2","sourceUrl":"assets/api/v1/animation-library/gamelab/dVaFR7XFVlGQX4d.e71iiKWvnLhMbaxG/category_food/american_pastrami.png","frameSize":{"x":355,"y":241},"frameCount":1,"looping":true,"frameDelay":2,"version":"dVaFR7XFVlGQX4d.e71iiKWvnLhMbaxG","categories":["food"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":355,"y":241},"rootRelativePath":"assets/api/v1/animation-library/gamelab/dVaFR7XFVlGQX4d.e71iiKWvnLhMbaxG/category_food/american_pastrami.png"},"043deebf-64b8-4795-be8d-db5055414f2f":{"name":"enemy3","sourceUrl":"assets/api/v1/animation-library/gamelab/YSis4_lex43su6FLaL__bhoag4eHAl8D/category_food/american_bbqribs.png","frameSize":{"x":388,"y":388},"frameCount":1,"looping":true,"frameDelay":2,"version":"YSis4_lex43su6FLaL__bhoag4eHAl8D","categories":["food"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":388,"y":388},"rootRelativePath":"assets/api/v1/animation-library/gamelab/YSis4_lex43su6FLaL__bhoag4eHAl8D/category_food/american_bbqribs.png"},"c26b6449-0b6c-4483-8486-107c0091c26e":{"name":"hero1","sourceUrl":"assets/api/v1/animation-library/gamelab/loycQXdICpzI4PfXITdIndG9GcVBmRdK/category_faces/kidportrait_01.png","frameSize":{"x":264,"y":368},"frameCount":1,"looping":true,"frameDelay":2,"version":"loycQXdICpzI4PfXITdIndG9GcVBmRdK","categories":["faces"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":264,"y":368},"rootRelativePath":"assets/api/v1/animation-library/gamelab/loycQXdICpzI4PfXITdIndG9GcVBmRdK/category_faces/kidportrait_01.png"},"3221caad-ae37-4eec-92a8-56bae4769e66":{"name":"b","sourceUrl":"assets/api/v1/animation-library/gamelab/IJemJVUh3J1gcGlCdIJ8obCWhXAqxbUT/category_backgrounds/kitchen.png","frameSize":{"x":400,"y":400},"frameCount":1,"looping":true,"frameDelay":2,"version":"IJemJVUh3J1gcGlCdIJ8obCWhXAqxbUT","categories":["backgrounds"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":400,"y":400},"rootRelativePath":"assets/api/v1/animation-library/gamelab/IJemJVUh3J1gcGlCdIJ8obCWhXAqxbUT/category_backgrounds/kitchen.png"},"c4e310e0-8174-4127-a46e-0cbcc94b488b":{"name":"dream","sourceUrl":null,"frameSize":{"x":386,"y":268},"frameCount":1,"looping":true,"frameDelay":12,"version":"Idc2NAa.RIoyyMm4SJd3fEZGksEyIlar","categories":["icons"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":386,"y":268},"rootRelativePath":"assets/c4e310e0-8174-4127-a46e-0cbcc94b488b.png"},"948cf14d-e690-40c9-85e8-9bdb31c4fb01":{"name":"city","sourceUrl":"assets/api/v1/animation-library/gamelab/OGMmhuYDozWZhjtNHQkjhdvs2Ge_Q0UF/category_backgrounds/city.png","frameSize":{"x":400,"y":400},"frameCount":1,"looping":true,"frameDelay":2,"version":"OGMmhuYDozWZhjtNHQkjhdvs2Ge_Q0UF","categories":["backgrounds"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":400,"y":400},"rootRelativePath":"assets/api/v1/animation-library/gamelab/OGMmhuYDozWZhjtNHQkjhdvs2Ge_Q0UF/category_backgrounds/city.png"}}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var gameState = "serve";
+
+
+var hero = createSprite(200,345,200,345);
+hero.shapeColor="red"
+
+var enemy1 = createSprite(200,250,10,10)
+enemy1.shapeColor="red"
+
+var enemy2 = createSprite(200,150,10,10)
+enemy2.shapeColor="red"
+
+var enemy3 = createSprite(200,50,10,10)
+enemy3.shapeColor="red"
+
+var net = createSprite(200,5,200,20)
+net.shapeColor="red"
+net.setAnimation("dream");
+net.scale=.15
+
+var Archivos =0;
+var Muertes = 0;
+
+hero.setAnimation("enemy1");
+hero.scale=.90;
+enemy1.setAnimation("hero");
+enemy1.scale=.80;
+enemy2.setAnimation("hero");
+enemy2.scale=.80;
+enemy3.setAnimation("hero");
+enemy3.scale=.80;
+
+enemy1.setVelocity(-10,0);
+enemy2.setVelocity(10,0);
+enemy3.setVelocity(-10,0);
+
+
+function draw() {
+background("green")  
+
+
+createEdgeSprites()
+
+
+
+
+enemy1.bounceOff(edges)
+enemy2.bounceOff(edges)
+enemy3.bounceOff(edges)
+
+if(keyDown(UP_ARROW)){
+  hero.y=hero.y-3
+}
+
+if(keyDown(DOWN_ARROW)){
+  hero.y=hero.y+3
+}
+
+if(keyDown(LEFT_ARROW)){
+  hero.x=hero.x-3
+}
+
+if(keyDown(RIGHT_ARROW)){
+  hero.x=hero.x+3
+}
+
+if(hero.isTouching(enemy1)|| hero.isTouching(enemy2)|| hero.isTouching(enemy3)){
+  playSound("assets/category_achievements/bubbly_game_achievement_sound.mp3")
+  hero.x=200
+  hero.y=350
+  Muertes = Muertes+1
+}
+if(hero.isTouching(net)){
+  playSound("assets/category_achievements/vibrant_game_game_gold_tresure_chest_open.mp3")
+  hero.x=200
+  hero.y=345
+  Archivos=Archivos+1
+}
+
+  if(gameState == "serve"){
+    text("presiona enter para comenzar, registra en el celular tu tiempo y ponlo en www.xinegames.com",10,222);
+    text("www.xinegamestudios.oddo.com",184,248);
+    stroke("red");
+    textSize("10");
+    
+  }
+
+    
+    if (Archivos==7){
+      gameState = "end";
+    }
+  
+  
+ 
+  
+
+ if (gameState == "end"){
+enemy1.setVelocity(0,0);
+enemy2.setVelocity(0,0);
+enemy3.setVelocity(0,0);
+stroke("red");
+text("fin del juego, puntuacion: "+Archivos,40,200);
+   }
+  
+
+    textSize(15);
+    stroke("red");
+    text("ARCHIVOS:"+Archivos,35,365);
+    
+
+    textSize(15);
+    stroke("red");
+    text("MUERTES"+Muertes,290,360);
+    
+    
+    
+drawSprites()
+}
+//HISTORIA
+//UN ROBOT LLAMADO ARNUBE TIENE QUE CARGAR SUS ARCHIVOS A LA NUBE PERO HAY UNOS VIRUS "TROYANOS,MALWARE
+// Y UN VIRUS LLAMADO WANNACRY" AYUDA AL ROBOT A CARGAR LOS ARCHIVOS A LA NUBE SIN TOCAR LOS VIRUS.  
+
+
+
+
+
+
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
